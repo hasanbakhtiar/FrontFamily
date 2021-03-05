45d54681522806ddbf04277c751059db0f93e11b
Hasan <hasan.doctype@gmail.com>
2021-03-05 16:43:54 +0400

    Jquery All Lesson Excample

---
+++ jqueryLessons/app.js
@@ -0,0 +1,65 @@
+$(function(){
+
+    
+    $("h1"/*nə ilə tətbiq edən*/)/*nə zaman tətbiq edən*/.click(function(){
+
+      $(this/*tetbiq olunan*/).hide()/*tetbiq eden hereket*/;
+
+
+
+    });
+  });
+
+// $(function(){
+//     $("button").click(function(){
+//       $("#soz").hide();  
+//     })
+// });
+
+
+
+// $(function(){
+//     $("button").dblclick(function(){
+//         $("p").css("color","red");
+//     });
+//     $("button").click(function(){
+//         $("p").css("color","green");
+//     });
+// });
+
+
+// // $(function(){
+//     $("button").dblclick(function(){
+//         $("p").css("color","red");
+//     });
+//     $("button").click(function(){
+//         $("p").css("color","green");
+//     });
+// });
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("div").animate({right: '250px'});
+//       $("div").animate({bottom: '250px'});
+//       $("div").animate({top: '550px'});
+//       $("div").animate({left: '550px'});
+      
+//     });
+//   });
+
+
+// $(document).ready(function(){
+//     $("#btn1").click(function(){
+//       $("#test1").text("Matrix");
+//     });
+//     $("#btn2").click(function(){
+//       $("#test2").html("<b>Front-Family</b>");
+//     });
+//     $("#btn3").click(function(){
+//       $("#test3").text      ("Reşad,Öznur,Nicat,Sevinc,Nərmin");
+//     });
+//   });
+
+
+
