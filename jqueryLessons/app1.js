6c1b91d0bae283179e94bcc6dd94f336c994868d
Hasan <hasan.doctype@gmail.com>
2021-03-17 17:14:39 +0400

    add reactLessons

---
+++ jqueryLessons/app1.js
@@ -0,0 +1,14 @@
+
+
+$(function() {
+    $("button").click(function(){
+        $( "div" ).css( "border", "2px solid red" )
+        .add( "p" )
+        .css( "background", "yellow" );
+    });   
+});
+
+
+
+
+
