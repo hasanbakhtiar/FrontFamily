99bbe922aa25a00fa51c30bbf4bc3642a1a0374d
Hasan <hasan.doctype@gmail.com>
2021-04-19 11:35:28 +0400

    github finder Search

--- ReactLessons/githubFinder/src/components/App.js
+++ ReactLessons/githubFinder/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 import Users from './Users'
+import Search from './Search'
 import axios from 'axios'
 
 
@@ -10,23 +11,26 @@ class App extends Component {
 
     constructor(props){
         super(props);
+        this.searchUsers = this.searchUsers.bind(this);
+
         this.state = {
             users: []
         }
     }
 
-    componentDidMount(){
+    searchUsers(keyword){
         
         axios
-             .get('https://api.github.com/users')
-             .then(res => this.setState({users: res.data}));
+             .get(`https://api.github.com/search/users?q=${keyword}`)
+             .then(res => this.setState({users: res.data.items}));
 
     }
 
     render() {
         return (
             <div>
-                <Navbar url="https://github.com" title="GitHub Finder" icon="fab fa-github" />
+                <Navbar url="https://github.com" title=" GitHub Finder" icon="fab fa-github" />
+                <Search searchUsers={this.searchUsers} />
                 <Users  users={this.state.users} />
             </div>
         )
